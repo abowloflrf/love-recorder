e5721f7ff59d53147926e24d1c8c733db0baa9c0
abowloflrf <abowloflrf@outlook.com>
2017-08-18 23:49:21 +0800

    Update home page. Still needs polish, so ugly...

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,29 @@
             </div>
         </div>
     </div>
+
+    <div class="love-time mt-3">
+        <h1 class="text-center">We have been together for:</h1>
+        <h1 class="text-center"><span id="love-time-d"></span> Days <span id="love-time-h"></span>Hours <span id="love-time-m"></span>Minutes <span id="love-time-s"></span>Seconds</h1>
+        <script>
+            var lovedate=new Date(2017,1,1);
+            setInterval(function(){
+                var datenow = new Date();
+                var duration=datenow-lovedate;
+                var duration_d=parseInt(duration/(1000*60*60*24));
+                var duration_h=parseInt((duration%(1000*60*60*24))/(3600*1000));
+                var duration_m=parseInt((duration%(1000*60*60))/(60*1000));
+                var duration_s=parseInt((duration%(1000*60))/(1000));
+                
+                document.getElementById('love-time-d').innerHTML=duration_d;
+                document.getElementById('love-time-h').innerHTML=duration_h;
+                document.getElementById('love-time-m').innerHTML=duration_m;
+                document.getElementById('love-time-s').innerHTML=duration_s;
+            },1000);
+            
+        </script>
+    </div>
+    
     <section id="cd-timeline" class="cd-container">
         @foreach($records as $record)
             @include('layouts.timeline-card')
