592bf7b37b03d0bd306313f5d1858bb4bf9d9103
abowloflrf <abowloflrf@outlook.com>
2017-09-16 21:09:01 +0800

    Some small chenges

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -90,11 +90,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://ruofeng.me">Ruofeng</a>
+                    <a href="https://github.com/abowloflrf/love-recorder">GitHub</a>
+                    <a href="https://laravel.com">Laravel</a>
                 </div>
             </div>
         </div>
