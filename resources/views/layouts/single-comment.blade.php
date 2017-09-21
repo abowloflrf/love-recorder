fb6c11172de44ae694830312f9c67836b7591799
abowloflrf <abowloflrf@outlook.com>
2017-09-21 22:03:54 +0800

    Comment board bug fixes

--- resources/views/layouts/single-comment.blade.php
+++ resources/views/layouts/single-comment.blade.php
@@ -1,5 +1,12 @@
  <div class="card mb-3">
-    <div class="card-header"><strong>{{$comment->display_name}}</strong> - <em><small class="text-muted">{{$comment->created_at}}</small></em></div>
+    <div class="card-header">
+        <strong>{{$comment->display_name}}</strong>
+        @if(Auth::guest())
+        @elseif(auth()->user()->member<3)
+            <small>{{$comment->email}}</small>
+        @endif
+         - <em><small class="text-muted">{{$comment->created_at}}</small></em>
+    </div>
     <div class="card-body">
         {{$comment->comment}}
     </div>
